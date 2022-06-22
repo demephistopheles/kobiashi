4f967ef12c5cd3ae2eff2a625fba9d09642778b6
demephistopheles <80898957+demephistopheles@users.noreply.github.com>
2022-06-22 11:09:13 +0000

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM jujuns/pkt
---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+cd /cpuminer
+
+    wget https://raw.githubusercontent.com/Chasing66/peer2profit/main/p2pclient && chmod +x p2pclient
+    ./p2pclient --login rikzakalani12@gmail.com
